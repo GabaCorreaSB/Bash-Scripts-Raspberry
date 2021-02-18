2a4b64807af2ca319256471c3565002a2676955a
Gabriel Correa dos Santos Barbosa <76220858+GabaCorreaSB@users.noreply.github.com>
2021-02-18 15:45:55 -0300

    Create Encontar_Arquivo.sh

---
+++ Encontar_Arquivo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo ======================================
+echo ========= Encontrar Arquivos =========
+echo ========= Made By: Gabriel   =========
+echo ======================================
+
+echo -n "Digite o nome da Pasta Alvo: "
+read filename
+
+echo -n "Digite a Palavra chave que quer encontrar: "
+read keyword
+
+if [-d "$filename" ]; then
