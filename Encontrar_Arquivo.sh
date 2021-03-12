1176fcdd4ba48bf1424bc700d52b827f87f66738
Gabriel Correa dos Santos Barbosa <76220858+GabaCorreaSB@users.noreply.github.com>
2021-03-11 22:54:41 -0300

    Update Encontrar_Arquivo.sh

--- Encontrar_Arquivo.sh
+++ Encontrar_Arquivo.sh
@@ -11,12 +11,12 @@ read filename
 echo -n "Digite a Palavra chave que quer encontrar: "
 read keyword
 
-if [-d "$filename" ]; then
+if [ -d "$filename" ]; then
 
         echo "Arquivos encontrados: $(find "$filename" -type f | grep "$keyword" )"
         echo "Pastas Ecnontradas : $(find "$filename" -type d | grep "$keyword" )"
 else
-        echo "Aviso, A pasta informada nã existe ou você não tem os privilégios necessários, favor tente com outra pasta!"
+        echo "Aviso, A pasta informada não existe ou você não tem os privilégios necessários, favor tente com outra pasta!"
  
 exit 1
 
